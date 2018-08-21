65b124f54be6214ea48ad6db34f5cc1d5b021073
paul.kwiek <paul@kwiek.net>
2018-08-21 10:49:48 -0500

    scope

---
+++ 06_address/main/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+
+	a := 43
+
+	fmt.Println("a - ", a)
+	fmt.Println("a's memory address - ", &a)
+	fmt.Printf("%d \n", &a)
+	var b  *int =  &a
+	fmt.Println(b)
+	fmt.Println(*b)
+
+}
